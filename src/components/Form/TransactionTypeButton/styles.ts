0f3e5421e017b2bdce35f23ea8a68a53079e31dd
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-12-27 17:19:52 -0300

    feat: create transaction type button

---
+++ src/components/Form/TransactionTypeButton/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+interface IconProps {
+  type: 'up' | 'down';
+}
+
+interface ContainerProps {
+  type: 'up' | 'down';
+  isActive: boolean;
+}
+
+export const Container = styled.TouchableOpacity<ContainerProps>`
+  width: ${RFPercentage(25)}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: 1.5px solid ${({ theme }) => theme.colors.text};
+  border-radius: 5px;
+  padding: 16px;
+
+  ${({ isActive, type }) => isActive && type === 'up' && css`
+    background-color: ${({ theme }) => theme.colors.success_light};
+    border: none;
+  `};
+
+  ${({ isActive, type }) => isActive && type === 'down' && css`
+    background-color: ${({ theme }) => theme.colors.attention_light};
+    border: none;
+  `};
+`;
+
+export const Icon = styled(Feather)<IconProps>`
+  font-size: ${RFValue(24)}px;
+  margin-right: 12px;
+  color: ${({ theme, type }) => 
+    type === 'up' ? theme.colors.success : theme.colors.attention
+  };
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`;
